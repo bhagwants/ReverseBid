43f1445a918ebd26e5104e9e1ffcf9c657d82993
bsidhu0707 <bsidhu0707@Sidhu-PC.VLI.com>
2015-01-13 22:18:00 -0500

    login changes

--- src/com/reversebid/service/dao/UserMongoDao.java
+++ src/com/reversebid/service/dao/UserMongoDao.java
@@ -1,8 +1,13 @@
 package com.reversebid.service.dao;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.shiro.authc.credential.DefaultPasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
 import com.reversebid.domain.configurable.User;
@@ -10,14 +15,33 @@ import com.reversebid.service.dao.interfaces.Dao;
 
 @Component
 public class UserMongoDao implements Dao{
+	
+	private final Log log = LogFactory.getLog(UserMongoDao.class);
 
 	@Autowired
 	private ApplicationContext applicationContext;
 	@Autowired
 	private MongoOperations mongoOperations;
 	
-	public User createUser(User user) {
-		mongoOperations.save(user);
+	public User createUser(User user) throws Exception{
+		try {
+			final DefaultPasswordService passwdService = new DefaultPasswordService();
+			user.setPassword(passwdService.encryptPassword(user.getPassword()));
+			mongoOperations.save(user);
+		} catch(Exception ex){
+			log.error("Error creating the user ", ex);
+			throw ex;
+		}
 		return user;
+	}
+	
+	public User getUser(String username) {
+		try {
+			return mongoOperations.findOne(new Query(Criteria.where("username").is(
+					username)), User.class);
+		} catch(Exception ex) {
+			log.error("Error finding user with user name : " + username, ex);
+		}
+		return null;
 	}
 }
